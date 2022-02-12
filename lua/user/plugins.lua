faeaf5b882deed374fc201ee114ca33f3930d3a6
straycatcoder <mathphobia@gmail.com>
2022-02-12 12:03:24 -0500

    autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,9 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter"
+  
+  -- colortheme
   use 'folke/tokyonight.nvim' -- colortheme
 
     -- cmp plugins
@@ -65,7 +68,13 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
-  
+
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
