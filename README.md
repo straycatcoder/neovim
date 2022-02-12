b456f44f3de57e50b98f38ae236ca04306d69af7
straycatcoder <mathphobia@gmail.com>
2022-02-12 10:53:14 -0500

    telescope

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Tutorial: https://github.com/LunarVim/Neovim-from-scratch
 n  <C-L>        @<Plug>NetrwRefresh                                   
 n  <C-L>       * <C-W>l
 ```
+* Telescope live-grep requires: https://github.com/BurntSushi/ripgrep 
+
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -34,3 +34,7 @@ keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+
