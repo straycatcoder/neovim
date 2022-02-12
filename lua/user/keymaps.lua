67d07169cb9b1c4c88dd217abd3563d86775e5b6
straycatcoder <mathphobia@gmail.com>
2022-02-12 12:52:20 -0500

    nvim tree

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -26,7 +26,8 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 -- c-l is not working, check with map command
 -- keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+-- keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
